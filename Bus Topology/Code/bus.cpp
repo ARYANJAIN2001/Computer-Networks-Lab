3fc5348af570424e4e4d838e16c7ceb65da50eaa
Ayushman Tripathy <ayushmantripathy9@gmail.com>
2021-10-16 15:22:04 +0530

    Added testcases for simulation

--- Bus Topology/Code/bus.cpp
+++ Bus Topology/Code/bus.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-int Node :: assign_id = -1;
-int Link :: assign_link_id = -1;
+int Node ::assign_id = -1;
+int Link ::assign_link_id = -1;
 
 set<int> all_mac_addresses;
-map<int,int> mac_address;
+map<int, int> mac_address;
 
 int assign_mac_address(int id)
 {
@@ -23,8 +23,8 @@ int assign_mac_address(int id)
     return new_addr;
 }
 
-void make_network(vector<Link *> &links, vector<Node *> &nodes);
-
+void make_network_1(vector<Link *> &links, vector<Node *> &nodes);
+void make_network_2(vector<Link *> &links, vector<Node *> &nodes);
 int main()
 {
 
@@ -32,25 +32,88 @@ int main()
     vector<Node *> nodes;
     vector<Link *> links;
 
-    make_network(links, nodes);
+    make_network_1(links, nodes);
+    
+    //  Unicast message sent by node 2 to node 5
+    nodes[2]->send_msg("Message from node 2 to 5", 1, mac_address[5]);
+    cout<<"............................................................................................................"<<endl;
+    usleep(2000000);
+    //  Broadcast message sent from node 6. It can't reach every other node in the network as all links are not bi-directional.
+    nodes[6]->send_msg("This is a broadcast message from node 6.", 1, 0);
+    cout<<"............................................................................................................"<<endl;
+    usleep(2000000);
 
-    cout<<"Network Ready !!"<<endl;
-    nodes[2]->send_msg("Hello. How are you?", 1, mac_address[7]);
+
+    //  if link 2 has failed , we simulate sending of a broadcast message from node 2 after link 2 has failed 
+    links[2]->enable_failure(); 
+        nodes[2]->send_msg("This is a broadcast message sent after link 2 has failed.", 1 , 0);
+        cout<<"............................................................................................................"<<endl;
+        usleep(2000000);
+
+
+    links[2]->disable_failure(); // restoring the link after simulating failure
+
+    //  if node 5 has failed 
+    nodes[5]->enable_failure();
+        
+        // we simulate sending a message from node 2 to node 5 first
+        nodes[2]->send_msg("Message from node 2 to node 5 after node failure.", 1 , mac_address[5]);
+        cout<<"............................................................................................................"<<endl;
+        usleep(2000000);
+
+        // we simulate sending a broadcast message from node 2 then (to show it only affects node 5)
+        nodes[2]->send_msg("Message from node 2 to node 5 after node failure.", 1 , 0);
+        cout<<"............................................................................................................"<<endl;
+        usleep(2000000);
+
+    nodes[5]->disable_failure();
+
+    cout<<"............................................................................................................"<<endl;
+    
+    make_network_2(links,nodes);
+    cout<<"New network ready for simulations."<<endl;
+
+    cout<<"............................................................................................................"<<endl;
+
+    //  sending a broadcast message from node 7 to all 
+    nodes[7]->send_msg("Broadcast message on network 2.", 1, 0);
+    cout<<"............................................................................................................"<<endl;
+    usleep(5000000);
+   
+    //  
+        links[0]->enable_failure();
+        links[2]->enable_failure();
+
+            //  sending a boadcast message from node 7 to all after failure of links 0 and 2.
+            nodes[7]->send_msg("Broadcast message on network 2.", 1, 0);
+            cout<<"............................................................................................................"<<endl;
+            usleep(2000000);
+
+        links[0]->disable_failure();
+        links[2]->disable_failure();
+    // 
+    
+
+
 
     return 0;
 }
 
-void make_network(vector<Link *> &links, vector<Node *> &nodes)
+void make_network_1(vector<Link *> &links, vector<Node *> &nodes)
 {
+    nodes.clear();
+    links.clear();
+    mac_address.clear();
+
     for (int i = 0; i < 10; i++)
     {
         nodes.push_back(new Node(assign_mac_address(i)));
     }
-    
-    links.push_back(new Link(0, {nodes[0],nodes[1],nodes[2]}));
-    links.push_back(new Link(1, {nodes[3],nodes[4],nodes[2]}));
-    links.push_back(new Link(2, {nodes[2],nodes[4],nodes[5], nodes[6], nodes[7]}));
-    links.push_back(new Link(0, {nodes[2],nodes[8],nodes[9]}));
+
+    links.push_back(new Link(0, {nodes[0], nodes[1], nodes[2]}));
+    links.push_back(new Link(1, {nodes[3], nodes[4], nodes[2]}));
+    links.push_back(new Link(2, {nodes[2], nodes[4], nodes[5], nodes[6], nodes[7]}));
+    links.push_back(new Link(0, {nodes[2], nodes[8], nodes[9]}));
 
     nodes[0]->insert_links({links[0]});
     nodes[1]->insert_links({links[0]});
@@ -62,4 +125,29 @@ void make_network(vector<Link *> &links, vector<Node *> &nodes)
     nodes[7]->insert_links({links[2]});
     nodes[8]->insert_links({links[3]});
     nodes[9]->insert_links({links[3]});
+}
+
+void make_network_2(vector<Link *> &links, vector<Node *> &nodes)
+{
+    nodes.clear();
+    links.clear();
+    mac_address.clear();
+    for (int i = 0; i < 8; i++)
+    {
+        nodes.push_back(new Node(assign_mac_address(i)));
+    }
+
+    links.push_back(new Link(2, {nodes[7], nodes[0], nodes[1], nodes[2], nodes[5]}));
+    links.push_back(new Link(0, {nodes[3], nodes[1], nodes[2], nodes[4], nodes[7], nodes[5], nodes[6]}));
+    links.push_back(new Link(1, {nodes[3], nodes[2], nodes[4], nodes[6], nodes[7]}));
+
+    nodes[0]->insert_links({links[0]});
+    nodes[1]->insert_links({links[0], links[1]});
+    nodes[2]->insert_links({links[0], links[1], links[2]});
+    nodes[3]->insert_links({links[1], links[2]});
+    nodes[4]->insert_links({links[1], links[2]});
+    nodes[5]->insert_links({links[0], links[1]});
+    nodes[6]->insert_links({links[1], links[2]});
+    nodes[7]->insert_links({links[0], links[1], links[2]});
+    
 }
--- Bus Topology/Code/network_components.hpp
+++ Bus Topology/Code/network_components.hpp
@@ -9,118 +9,151 @@ using namespace std;
 
 class Link;
 
-class Node{
-    static int assign_id; 
-    
-    public:
-        int mac_addr;
-        int device_id;
-        vector<Link *> links;
+class Node
+{
+    static int assign_id;
 
-        Node(int mac_address){
-            mac_addr = mac_address;
-            device_id = ++assign_id;
-        }
-        
-        void insert_links(vector< Link*> links);
-        void send_msg(string msg, int seq_no, int mac_addr_dest);
-        void receive_frame(DataFrame data);
+public:
+    int mac_addr;
+    int device_id;
+    vector<Link *> links;
+
+    bool failure = false;
+
+    Node(int mac_address)
+    {
+        mac_addr = mac_address;
+        device_id = ++assign_id;
+    }
+
+    void insert_links(vector<Link *> links);
+    void send_msg(string msg, int seq_no, int mac_addr_dest);
+    void receive_frame(DataFrame data);
+    void enable_failure();
+    void disable_failure();
 };
 
 class Link
 {
     static int assign_link_id;
 
-    private:
-        vector<Node *> nodes;
-        int direction_of_transfer;
-        int link_id;
+private:
+    vector<Node *> nodes;
+    int direction_of_transfer;
+    int link_id;
 
-    public:
-        Link(int direction, vector< Node *> nodes){
-            direction_of_transfer = direction;
-            for(int i = 0 ; i < nodes.size() ; ++i){
-                insert_node(nodes[i]);
+    bool failure = false;
+
+public:
+    Link(int direction, vector<Node *> nodes)
+    {
+        direction_of_transfer = direction;
+        for (int i = 0; i < nodes.size(); ++i)
+        {
+            insert_node(nodes[i]);
+        }
+
+        link_id = ++assign_link_id;
+    }
+
+    void insert_node(Node *n)
+    {
+        for (int i = 0; i < nodes.size(); i++)
+        {
+            if (nodes[i]->mac_addr == n->mac_addr)
+            {
+                cout << "Cannot connect to the same link twice" << endl;
+                return;
             }
+        }
+        nodes.push_back(n);
+    }
 
-            link_id = ++assign_link_id;
+    void enable_failure()
+    {
+        failure = true;
+    }
+
+    void disable_failure()
+    {
+        failure = false;
+    }
+
+    void send_frame_in_link(DataFrame data)
+    {
+        if (failure)
+        {
+            cout << "Link " << link_id << " has failed. Can't send message in this link." << endl;
+            return;
         }
 
-        void insert_node(Node* n)
+        int node_id;
+        for (int i = 0; i < nodes.size(); ++i)
         {
-            nodes.push_back(n);
-        }
-
-
-        void send_frame_in_link(DataFrame data)
-        {
-
-            int node_id;
-            for(int i = 0 ; i < nodes.size() ; ++i)
+            if (data.mac_addr_source == nodes[i]->mac_addr)
             {
-                if(data.mac_addr_source == nodes[i]->mac_addr){
-                    node_id = i;
-                    break;
-                }
-            }
-
-            // cout<<"Sending in Link : "<<link_id<<endl;
-
-            switch (direction_of_transfer)
-            {
-
-                case 0: // bidirectional transfer can happen
-                {
-                    vector<thread *> transfer_thread;
-
-                    transfer_thread.push_back(new thread(&Link::send_to_left, this, data, node_id));
-                    transfer_thread.push_back(new thread(&Link::send_to_right, this, data, node_id));
-
-                    for (int i = 0; i < transfer_thread.size(); ++i)
-                        transfer_thread[i]->join();
-
-                    break;
-                }
-
-                case 1: // tranfer can happen to left only
-                {
-                    send_to_left(data, node_id);
-                    break;
-                }
-                case 2: // transfer can happen to right only
-                {
-                    send_to_right(data, node_id);
-                    break;
-                }
-                default:
-                    break;
-                }
-        }
-
-        void send_to_left(DataFrame data, int node_id)
-        {
-            for (int i = node_id - 1; i >= 0; --i)
-            {
-                cout<<"Link from which message was received: "<<link_id<<endl;
-                nodes[i]->receive_frame(data);
-                usleep(500000);
+                node_id = i;
+                break;
             }
         }
 
-        void send_to_right(DataFrame data, int node_id)
+        // cout<<"Sending in Link : "<<link_id<<endl;
+
+        switch (direction_of_transfer)
         {
-            for (int i = node_id + 1; i < nodes.size(); ++i)
-            {
-                cout<<"Link from which message was received: "<<link_id<<endl;
-                nodes[i]->receive_frame(data);
-                usleep(500000);
-            }
+
+        case 0: // bidirectional transfer can happen
+        {
+            vector<thread *> transfer_thread;
+
+            transfer_thread.push_back(new thread(&Link::send_to_left, this, data, node_id));
+            transfer_thread.push_back(new thread(&Link::send_to_right, this, data, node_id));
+
+            for (int i = 0; i < transfer_thread.size(); ++i)
+                transfer_thread[i]->join();
+
+            break;
         }
+
+        case 1: // tranfer can happen to left only
+        {
+            send_to_left(data, node_id);
+            break;
+        }
+        case 2: // transfer can happen to right only
+        {
+            send_to_right(data, node_id);
+            break;
+        }
+        default:
+            break;
+        }
+    }
+
+    void send_to_left(DataFrame data, int node_id)
+    {
+        for (int i = node_id - 1; i >= 0; --i)
+        {
+            cout << "Link from which message was received: " << link_id << endl;
+            nodes[i]->receive_frame(data);
+            usleep(500000);
+        }
+    }
+
+    void send_to_right(DataFrame data, int node_id)
+    {
+        for (int i = node_id + 1; i < nodes.size(); ++i)
+        {
+            cout << "Link from which message was received: " << link_id << endl;
+            nodes[i]->receive_frame(data);
+            usleep(500000);
+        }
+    }
 };
 
-void Node :: send_msg(string msg, int seq_no, int mac_addr_dest)
+void Node ::send_msg(string msg, int seq_no, int mac_addr_dest)
 {
-    cout<< "Sending message now."<<endl;
+    cout << "Sending message now." << endl;
 
     DataFrame *data = new DataFrame(mac_addr, mac_addr_dest, msg, seq_no);
     vector<thread *> send_to_links;
@@ -128,7 +161,7 @@ void Node :: send_msg(string msg, int seq_no, int mac_addr_dest)
     for (int i = 0; i < links.size(); i++)
     {
         send_to_links.push_back(new thread(&Link::send_frame_in_link, links[i], *data));
-        usleep(50);
+        usleep(500);
     }
 
     for (int i = 0; i < links.size(); i++)
@@ -137,13 +170,17 @@ void Node :: send_msg(string msg, int seq_no, int mac_addr_dest)
     }
 }
 
-void Node :: receive_frame(DataFrame data)
+void Node ::receive_frame(DataFrame data)
 {
+    if(failure){
+        cout<<"Node "<<device_id<<" : Can't receive message."<<endl;
+        return;
+    }
     cout << device_id << " : Data Frame with seq_no: " << data.seq_no << " Received." << endl;
 
-    if (data.mac_addr_dest == mac_addr || data.mac_addr_dest == 0 )
+    if (data.mac_addr_dest == mac_addr || (data.mac_addr_dest == 0 && data.mac_addr_source != mac_addr))
     {
-        cout << device_id << " : Data Frame with seq_no: " << data.seq_no << " Accepted. Message Received : "<< data.message << endl;
+        cout << device_id << " : Data Frame with seq_no: " << data.seq_no << " Accepted. Message Received : " << data.message << endl;
     }
     else
     {
@@ -151,11 +188,20 @@ void Node :: receive_frame(DataFrame data)
     }
 }
 
-void Node :: insert_links(vector<Link*> network_link)
+void Node ::insert_links(vector<Link *> network_link)
 {
-    for(int i = 0 ; i < network_link.size() ; ++i){
+    for (int i = 0; i < network_link.size(); ++i)
+    {
         links.push_back(network_link[i]);
     }
 }
 
+void Node :: enable_failure()
+{
+    failure = true;
+}
 
+void Node :: disable_failure()
+{
+    failure = false;
+}
